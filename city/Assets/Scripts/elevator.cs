8ab8ba15be4ef9d795f289b9ab002de95498edfe
Monica <olanescumonica98@yahoo.com>
2020-03-06 09:47:57 +0200

    city added

---
+++ city/Assets/Scripts/elevator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class elevator : MonoBehaviour
+{
+    public GameObject movePlatform;
+    public GameObject player;
+
+    private void OnTriggerStay()
+    {
+        movePlatform.transform.position += movePlatform.transform.up * Time.deltaTime;
+        player.transform.position += player.transform.up * Time.deltaTime;
+    }
+}
