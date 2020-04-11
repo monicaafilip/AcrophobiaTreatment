0bbc4a856351b1b03f7db59cf525dcc32104a0a1
Monica Olanescu <olanescu_monica@yahoo.com>
2020-04-11 19:01:31 +0300

    gaze icon appeard only when watching the canvas + problems with higlighiting the buttons

---
+++ AcTreatment/Assets/Scripts/menu/gazeAppears.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class gazeAppears : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "UI")
+            GetComponent<SpriteRenderer>().enabled = true;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        // /if(other.tag == "UI")
+            GetComponent<SpriteRenderer>().enabled = false;
+    }
+    
+}
